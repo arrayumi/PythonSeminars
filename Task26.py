cb09306605e56b68d7d52de8049a702eae84c0d6
Anna <thoria@yandex.ru>
2022-12-01 12:36:46 +0300

    Task26 added

--- Task26.py
+++ Task26.py
@@ -2,3 +2,31 @@
 # *Пример:*
 # - для k = 8 список будет выглядеть так: 
 # [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] [Негафибоначчи]
+
+k = 8
+
+def list_pos(num):
+    list = []
+    list.append(0)
+    list.append(1)
+    for i in range(2, num+1):
+        list.append(list[i-1] + list[i-2])
+    return list
+
+def list_neg(num):
+    list = []
+    list.append(1)
+    list.append(-1)
+    num1, num2 = list[0], list[1]
+    for i in range(2, num):
+        num1, num2 = num2, num1 - num2
+        list.append(num2)
+    list.reverse()
+    return list
+
+def final_list (list_neg, list_pos):
+    final_list = list_neg + list_pos
+    return final_list
+
+res = final_list(list_neg(k), list_pos(k))
+print(res)
